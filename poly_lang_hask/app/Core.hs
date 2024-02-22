b54d982c13745526903cfc52f1d22c5462b1fa15
IModer <korpa.peter0723@gmail.com>
2024-02-22 11:31:36 +0100

    Added typing

--- poly_lang_hask/app/Core.hs
+++ poly_lang_hask/app/Core.hs
@@ -5,18 +5,65 @@ import Data.Maybe
 type Name = String
 
 {-
--- De Bruijn index
-newtype Ix  = Ix  Int 
-    deriving (Eq, Show, Num) via Int
+-- TODO : 
+    - builting functions for Int Bool, (+), (*), ...
+    - More basetypes : Int, Rac, Real, ...
+    - More tests : 
+        - for Bool, Int
+        - for just general lambdacalc (like well typed combinators)
 
--- De Bruijn level
-newtype Lvl = Lvl Int 
-    deriving (Eq, Show, Num) via Int
+    - Maybe : merge TTm and Tm
 -}
+
+-- FOR NOW: trick:
+--  TTm is a typed term, 
+--  we type check it then we translate it into Tm
+
+data TTm
+    = TVar Name
+    | TLam Name Type TTm
+    | TApp TTm TTm
+    | TLet Name TTm TTm
+    | TLit Literal
+    deriving Show
+
+data Type 
+    = TArr Type Type
+    | TInt
+    | TBool
+    deriving (Show, Eq)
+
+type TEnv = [(Name, Type)]
+
+--- Type checking
+
+-- TODO: Proper Error monad instead of Maybe
+typeCheck :: TEnv -> TTm -> Maybe Type
+typeCheck _ (TLit (LInt  a)) = Just TInt
+typeCheck _ (TLit (LBool a)) = Just TBool
+typeCheck env (TVar x) = (lookup x env)
+typeCheck env (TLam x t e) = case (typeCheck ((x,t):env) e) of
+    Just t' -> Just $ TArr t t'
+    Nothing -> Nothing
+typeCheck env (TApp e1 e2) = do
+    t1 <- typeCheck env e1
+    t2 <- typeCheck env e2
+    case t1 of
+        (TArr t t') | t == t2 -> return t'
+        (TArr t _)            -> Nothing
+        ty                    -> Nothing
+
+loseType :: TTm -> Tm
+loseType (TVar n)     = Var n
+loseType (TLam n t e) = Lam n (loseType e)
+loseType (TApp t u)   = App (loseType t) (loseType u)
+loseType (TLet n t u) = Let n (loseType t) (loseType u)
+loseType (TLit l)     = Lit l
+
+---
 
 data Tm 
     = Var Name
---    | Lam Name Type Tm
     | Lam Name Tm
     | App Tm Tm
     | Let Name Tm Tm
@@ -28,18 +75,15 @@ data Literal
     | LBool Bool 
     deriving Show
 
-data Type 
-    = TArr Type Type
-    | TInt
-    | TBool
-    deriving Show
-
 type Env = [(Name, Val)]
 
 data Val
     = VVar Name
     | VApp Val Val
     | VLam Name (Val -> Val)
+    | VLit Literal
+
+--- Evaluation
 
 freshName :: [Name] -> Name -> Name
 freshName ns x = if elem x ns 
@@ -55,11 +99,13 @@ evalTerm env (Var n)     = fromJust $ lookup n env  --TODO : not fromJust
 evalTerm env (App t u)   = vLamApp (evalTerm env t) (evalTerm env u)
 evalTerm env (Lam n t)   = VLam n (\u -> evalTerm ((n, u):env) t)
 evalTerm env (Let n t u) = evalTerm ((n, evalTerm env t):env) u
+evalTerm env (Lit l)     = VLit l
+-- Here be builtin functions
 
 quoteTerm :: [Name] -> Val -> Tm
-quoteTerm ns (VVar x)     = Var x
+quoteTerm _  (VVar x)     = Var x
 quoteTerm ns (VApp t u)   = App (quoteTerm ns t) (quoteTerm ns u)
---Question what goes here ^
+quoteTerm ns (VLit l)     = Lit l
 quoteTerm ns (VLam x t)   = Lam fx (quoteTerm (fx:ns) (t (VVar x)))
     where
         fx = freshName ns x
@@ -67,7 +113,13 @@ quoteTerm ns (VLam x t)   = Lam fx (quoteTerm (fx:ns) (t (VVar x)))
 normalForm :: Env -> Tm -> Tm
 normalForm env tm = quoteTerm (map fst env) $ evalTerm env tm
 
--- Tests
+runTypedTerm :: TTm -> Maybe Tm
+runTypedTerm tm = case typeCheck [] tm of
+    Just t  -> Just $ normalForm [] $ loseType tm
+    Nothing -> Nothing
+
+--- Tests
+
 test1 = normalForm [] (Lam "x" (App (Var "x") (Var "x")) )
 
 idTm = (Lam "x" (Var "x"))
@@ -78,3 +130,11 @@ idTm = (Lam "x" (Var "x"))
 test2 = (App idTm test1)
 
 test3 = (Let "x" (idTm) (App test1 (Var "x")))
+
+boolId = (TLam "b" TBool (TVar "b")) -- :: TArr TBool TBool 
+
+true = TLit (LBool True)             -- :: TBool 
+
+trueAgain = TApp boolId true         -- :: TBool
+
+runTrueAgain = runTypedTerm trueAgain -- == Just (Lit (LBool True))
