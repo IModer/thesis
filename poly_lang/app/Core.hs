df4bdcd880cb7cae3a229edab57c43ddf675e7e0
IModer <korpa.peter0723@gmail.com>
2024-03-05 09:27:24 +0100

    Start work on new Core with State

--- poly_lang/app/Core.hs
+++ poly_lang/app/Core.hs
@@ -3,234 +3,83 @@
 module Core where
 
 import Data.Maybe
+import Control.Monad.Trans       --lift
+import Control.Monad.State.Class --MonadClass
+import Control.Monad.State.Lazy  --StateT
+
 
 type Name = String
 
-{-
--- So far: STLC with
-    Bool and Int types
-    Let bindings
-    with buildin functions for Bool and Nat: (+), (*), And, Or
-
--- TODO :
-    - More basetypes : Rac, Real, Comp
-    - And subtyped typchecking
-    - More tests:
-        - for Bool, Int
-        - for just general lambdacalc (like well typed combinators)
-
--}
-
--- FOR NOW: trick:
---  TTm is a typed term, 
---  we type check it then we translate it into Tm
-
-data TTm
-    = TVar Name
-    | TLam Name Type TTm
-    | TApp TTm TTm
-    | TLet Name TTm TTm
-    | TLit Literal
-    | TPlus TTm TTm         -- (i: Int)   +  (j : Int)
-    | TTimes TTm TTm        -- (i: Int)   *  (j : Int)
-    | TAnd  TTm TTm         -- (b : Bool) && (l :Bool)
-    | TOr  TTm TTm          -- (b : Bool) || (l :Bool)
-    deriving Show
-
-data Type
-    = TArr Type Type
-    | TInt
-    | TBool
-    | TTop
-    deriving (Show, Eq)
-
 type TEnv = [(Name, Type)]
+type Env  = [(Name,TTm)]
 
---- Type checking
+data SEnv = SEnv {typeEnv :: TEnv , 
+                  nameEnv :: Env}
 
--- TODO: Proper Error monad instead of Maybe
-typeCheck :: TEnv -> TTm -> Maybe Type
-typeCheck env = \case
-    TLit (LInt  _) -> Just TInt
-    TLit (LBool _) -> Just TBool
-    TLit (LTop)    -> Just TTop
-    TVar x         -> (lookup x env)  -- Error can occure here: "Undefined variable x"
+
+--eval :: SEnv -> String -> (SEnv, String)
+--eval :: MonadState SEnv m => String -> m String
+eval :: String -> State SEnv String --which is :: String -> StateT SEnv Ident String 
+eval cs = case parseString cs of
+    Left a   -> return "Parse error"  --TODO : prettyPrint
+    Right tm -> helper (runTypedTerm' tm) -- REALLY TODO :: StateT SEnv Maybe String -> State SEnv String
+        -- This could be return $ maybe "Type error" prettyPrint tm'
+        
+        --return $ maybe "Type error" prettyPrint (runTypedTerm' tm)
+        
+        --return $ case tm' of
+        --    Just tm'' -> prettyPrint tm''
+        --    Nothing   -> "Type error"
+        where 
+            helper :: StateT SEnv Maybe Tm -> State SEnv String
+            helper st = do
+                env <- get
+                mapStateT (\case 
+                    (Just (tm',env')) -> return (prettyPrint tm',env')
+                    Nothing -> return $ ("Type error",env) ) st
+
+--runTypedTerm' :: SEnv -> TTm -> (SEnv, Maybe Tm)
+-- TTm -> State SEnv (Maybe Tm)
+-- Ha failelt a typeCheck akkor nem kéne az új envet tovább adni
+-- Amúgy StateT Maybe SEnv Tm
+
+--runTypedTerm' :: MonadState SEnv m => TTm -> m (Maybe Tm)
+runTypedTerm' :: TTm -> StateT SEnv Maybe Tm
+runTypedTerm' tm = do
+    t <- typeCheck' tm
+    (state . runState) $ normalForm' (loseType tm)
+
+-- TTm -> Maybe Type
+-- SEnv -> TTm -> (SEnv, Maybe Type)
+-- TTm -> State SEnv (Maybe Type)
+--typeCheck' :: MonadState SEnv m => TTm -> m (Maybe Type)
+typeCheck' :: TTm -> StateT SEnv Maybe Type
+typeCheck' = \case
+    TLit (LInt  _) -> return TInt
+    TLit (LBool _) -> return TBool
+    TLit LTop      -> return TTop
+    TVar x         -> undefined
     TLet x e u     -> do
-        t <- typeCheck env e
-        typeCheck ((x,t):env) u
-    TLam x t e     -> do
-        t' <- typeCheck ((x,t):env) e
-        return $ TArr t t'
-    TApp e1 e2     -> do
-        t1 <- typeCheck env e1
-        t2 <- typeCheck env e2
-        case t1 of
-            (TArr t t') | t == t2 -> return t'
-            (TArr _ _)            -> Nothing
-            _                     -> Nothing
-    TPlus e1 e2    -> bothTypesEqual env e1 e2 TInt
-    TTimes e1 e2   -> bothTypesEqual env e1 e2 TInt
-    TAnd e1 e2     -> bothTypesEqual env e1 e2 TBool
-    TOr e1 e2      -> bothTypesEqual env e1 e2 TBool
+        t <- typeCheck' e
+        modify (\(SEnv tenv env) -> SEnv ((x,t) : tenv) env)
+        typeCheck' u
+    TLam x t e     -> undefined
+    TApp e1 e2     -> undefined
+    TPlus e1 e2    -> undefined
+    TTimes e1 e2   -> undefined
+    TAnd e1 e2     -> undefined
+    TOr e1 e2      -> undefined
 
-bothTypesEqual :: TEnv -> TTm -> TTm -> Type -> Maybe Type
-bothTypesEqual env e1 e2 t  = do
-    t1 <- typeCheck env e1
-    t2 <- typeCheck env e2
-    if t1 == t then (if t2 == t then Just t else Nothing) else Nothing
+--normalForm' :: SEnv -> Tm -> (SEnv, Tm)
+normalForm' :: Tm -> State SEnv Tm
+--normalForm' :: MonadState SEnv m => Tm -> m Tm
+normalForm' = undefined
 
-loseType :: TTm -> Tm
-loseType = \case
-    TVar n     -> Var n
-    TLam n _ e -> Lam n (loseType e)
-    TApp t u   -> App (loseType t) (loseType u)
-    TLet n t u -> Let n (loseType t) (loseType u)
-    TLit l     -> Lit l
-    TPlus t u  -> Plus (loseType t) (loseType u)
-    TAnd t u   -> And (loseType t) (loseType u)
-    TTimes t u -> Times (loseType t) (loseType u)
-    TOr t u    -> Or (loseType t) (loseType u)
+--quoteTerm' :: SEnv -> Val -> (SEnv, Tm) 
+--quoteTerm' :: Val -> State SEnv Tm
+quoteTerm' :: MonadState SEnv m => Val -> m Tm
+quoteTerm' = undefined
 
---- Evaluation
-
-data Tm
-    = Var Name
-    | Lam Name Tm
-    | App Tm Tm
-    | Let Name Tm Tm
-    | Lit Literal
-    | Plus Tm Tm
-    | Times Tm Tm
-    | And Tm Tm
-    | Or Tm Tm
-    deriving Show
-
-data Literal
-    = LInt Int
-    | LBool Bool
-    | LTop
-    deriving Show
-
-type Env = [(Name, Val)]
-
-data Val
-    = VVar Name
-    | VApp Val Val
-    | VLam Name (Val -> Val)
-    | VLit Literal
-
-freshName :: [Name] -> Name -> Name
-freshName ns x = if elem x ns
-                    then freshName ns (x ++ "'")
-                    else x
-
-vLamApp :: Val -> Val -> Val
-vLamApp (VLam _ t) u = t u
-vLamApp t          u = VApp t u
-
-evalTerm :: Env -> Tm -> Val
-evalTerm env = \case
-    Var n                                   -> fromJust $ lookup n env -- NOTE: fromJust is safe
-    App t u                                 -> vLamApp (evalTerm env t) (evalTerm env u)
-    Lam n t                                 -> VLam n (\u -> evalTerm ((n, u):env) t)
-    Let n t u                               -> evalTerm ((n, evalTerm env t):env) u
-    Lit l                                   -> VLit l
-    Plus  u                 t               -> case evalTerm env u of
-        VLit (LInt i)    -> case evalTerm env t of
-            VLit (LInt j)    -> VLit $ LInt $ i + j
-            _                -> error "You shouldn't be here ..."
-        _                -> error "You shouldn't be here ..."
-    Times u                 t               -> case evalTerm env u of
-        VLit (LInt i)    -> case evalTerm env t of
-            VLit (LInt j)    -> VLit $ LInt $ i * j
-            _                -> error "You shouldn't be here ..."
-        _                -> error "You shouldn't be here ..."
-    And   u                 t               -> case evalTerm env u of
-        VLit (LBool i)    -> case evalTerm env t of
-            VLit (LBool j)   -> VLit $ LBool $ i && j
-            _                -> error "You shouldn't be here ..."
-        _                -> error "You shouldn't be here ..."
-    Or    u                 t               -> case evalTerm env u of
-        VLit (LBool i)    -> case evalTerm env t of
-            VLit (LBool j)   -> VLit $ LBool $ i || j
-            _                -> error "You shouldn't be here ..."
-        _                -> error "You shouldn't be here ..."
-
--- Here be builtin functions
-
-quoteTerm :: [Name] -> Val -> Tm
-quoteTerm ns = \case
-    VVar x                      -> Var x
-    VApp t u                    -> App (quoteTerm ns t) (quoteTerm ns u)
-    VLit l                      -> Lit l
-    VLam (freshName ns -> x) t  -> Lam x (quoteTerm (x:ns) (t (VVar x)))
-
-normalForm :: Env -> Tm -> Tm
-normalForm env tm = quoteTerm (map fst env) $ evalTerm env tm
-
-runTypedTerm :: TTm -> Maybe Tm
-runTypedTerm tm = do
-    _ <- typeCheck [] tm
-    return $ normalForm [] $ loseType tm
-
---- Pritty print
-
-prettyPrint :: Tm -> String
-prettyPrint = \case
-    Var n         -> n
-    App t u       -> unwords ["(",prettyPrint t,prettyPrint u,")"]
-    Lam n t       -> unwords ["(f",n,"->",prettyPrint t,")"]
-    Let n t u     -> unwords ["(let",n,"=",prettyPrint t,prettyPrint u,")"]
-    Lit (LBool l) -> show l
-    Lit (LInt l)  -> show l
-    Lit (LTop)    -> "()"  --TODO : we can make this empty, its only () for debug
-    Plus t u      -> unwords [prettyPrint t,"+",prettyPrint u]
-    Times t u     -> unwords [prettyPrint t,"+",prettyPrint u]
-    And t u       -> unwords [prettyPrint t,"+",prettyPrint u]
-    Or t u        -> unwords [prettyPrint t,"+",prettyPrint u]
-
---- Tests
-
--- TODO : Organise tests 
-
-test1 :: Tm 
-test1 = normalForm [] (Lam "x" (App (Var "x") (Var "x")) )
-
-idTm :: Tm
-idTm = (Lam "x" (Var "x"))
-
--- Only for Untyped cos in STLC Y is not well typed
---yTm = (Lam "f" (App (Lam "x" (App (Var "f") (App (Var "x") (Var "x")))) (Lam "x" (App (Var "f") (App (Var "x") (Var "x"))))))
-
-test2 :: Tm
-test2 = (App idTm test1)
-
-test3 :: Tm
-test3 = (Let "x" (idTm) (App test1 (Var "x")))
-
-boolId :: TTm
-boolId = (TLam "b" TBool (TVar "b")) -- :: TArr TBool TBool
-
-true :: TTm
-true = TLit (LBool True)             -- :: TBool
-
-trueAgain :: TTm
-trueAgain = TApp boolId true         -- :: TBool
-
-runTrueAgain :: Maybe Tm 
-runTrueAgain = runTypedTerm trueAgain -- == Just (Lit (LBool True))
-
-addTest :: Int -> Int -> TTm
-addTest x y = (TPlus (TLit (LInt x)) (TLit (LInt y)))
-
-testTm1 :: TTm
-testTm1 = TApp (TLam "x" TBool (TLit (LBool True))) (TLit (LBool False))
-
-typeCheckTest :: Maybe Type
-typeCheckTest = typeCheck [] testTm1
-
-badTm :: TTm
-badTm = TPlus (TLit (LInt 3)) (TPlus (TLit (LInt 3)) (TLit (LInt 3)))
-
-unreachableCodeHasBeenReached :: Tm
-unreachableCodeHasBeenReached = normalForm [] (Plus (Lit (LInt 3)) (Lit (LBool True)) )
+--evalTerm' :: Env -> Tm -> (Env, Val)
+evalTerm' :: MonadState SEnv m => Tm -> m Val
+evalTerm' = undefined
--- poly_lang/app/Main.hs
+++ poly_lang/app/Main.hs
@@ -69,92 +69,44 @@ read_ = do
     hFlush stdout
     getLine
 
---eval_ :: String -> String
 -- Handle repl option like : :q - quit, :h - help, ...
 eval_ :: String -> String
 eval_ (':':'h':_) = "This is a help"
 eval_ cs          = case parseString cs of
     Left a  -> errorBundlePretty a
-    Right t -> case runTypedTerm t of 
-        Just t' -> prettyPrint t' 
-        Nothing -> "Typechecking failed"
-{- TODO
-eval_ cs          = do 
-    term <- parseTerm cs
-    normalizeTerm term
--}
+    Right t -> maybe
+                "Typechecking failed"
+                prettyPrint
+                $ runTypedTerm t
 
 print_ :: String -> IO ()
 print_ = putStrLn
+
+runRepl' :: IO ()
+runRepl' = evalStateT runStatefulRepl $ SEnv [] []
+
+runStatefulRepl :: StateT SEnv IO ()
+runStatefulRepl = do
+    inp <- lift read_                           -- Lift IO into StateT IO
+    unless (inp == ":q") $ do
+        tm <- (state . runState) $ eval inp     -- Unbox and box (StateT Id -> StateT IO)
+        lift $ print_ tm                        -- Lift IO into StateT IO
+        runStatefulRepl
 
 
 -----------------------------------------------------------------------
 ---------------------------Playing with State--------------------------
 -----------------------------------------------------------------------
 
-type SEnv = (TEnv , Env) 
-
 runRepl' :: IO ()
-runRepl' = runStatefulRepl ([],[])
+runRepl' = evalStateT runStatefulRepl $ SEnv [] []
 
-runStatefulRepl :: SEnv -> IO ()
-runStatefulRepl env = do
-    inp <- read_
+-- This should maybe be StateT IO
+--runStatefulRepl :: SEnv -> IO ()
+runStatefulRepl :: StateT SEnv IO ()
+runStatefulRepl = do
+    inp <- lift read_                           -- Lift IO into StateT IO
     unless (inp == ":q") $ do
-        let (env', tm) = eval env inp in  -- This could fail, for now let it be String
-            do 
-                print_ tm
-                runStatefulRepl env'
-
-eval :: SEnv -> String -> (SEnv, String)
-eval env cs = case parseString cs of
-    Left a    -> (env, "Parse error")
-    Right tm  -> case runTypedTerm' env tm of
-        (env', Just tm') -> (env', prettyPrint tm')
-        (env', Nothing)  -> (env', "Typechecking error")
-
-runTypedTerm' :: SEnv -> TTm -> (SEnv, Maybe Tm)
-runTypedTerm' env tm = do
-    let (env', t) = typeCheck' env tm in
-        case t of 
-            Nothing -> (env, Nothing)  -- régi env, mert failelt a typeCheck
-            Just t -> let (env'', tm') = normalForm' env' $ loseType tm in (env'', Just tm')
-
-typeCheck' :: SEnv -> TTm -> (SEnv, Maybe Type)
-typeCheck' = undefined
-
--- :: Tm -> State SEnv Tm
-normalForm' :: SEnv -> Tm -> (SEnv, Tm)
-normalForm' = undefined
-
--- :: Val -> State SEnv Tm
-quoteTerm' :: SEnv -> Val -> (SEnv, Tm) 
-quoteTerm' = undefined
-{-
-
-runTypedTerm :: Env -> TTm -> (Env, Maybe Tm)
-runTypedTerm env tm = do
-    _ <- typeCheck env tm
-    return $ normalForm env $ loseType tm
-
-
-typeCheck most :: TEnv -> TTm -> Maybe Type
-
-typeCheck :: Env -> TTm -> (Env, Maybe Type)
-typeChekk (tenv, env) = ... (csak tenv et használja és módosítja)
-
-normalForm :: Env -> Tm -> (Env, Tm)
-
-quoteTerm :: Env -> Val -> (Env, Tm)   -- ő pl nem módosítja az Env-et
-qouteTerm env = ... (mostani kód)
-    where
-        ns = map (fst . fst) env
-
-evalTerm :: Env -> Tm -> (Env, Val)
-
-print_ .: String -> IO ()
-print_ = putStrLn
-
-
-
--}
+        tm <- (state . runState) $ eval inp     -- Unbox and box (StateT Id -> StateT IO)
+        lift $ print_ tm                        -- Lift IO into StateT IO
+        runStatefulRepl
