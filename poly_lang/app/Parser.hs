2bf7af4e0181e9f2d626ffb474960b5f2d1c42d0
IModer <korpa.peter0723@gmail.com>
2024-03-13 10:53:16 +0100

    Moved to new Parser

--- poly_lang/app/Parser.hs
+++ poly_lang/app/Parser.hs
@@ -1,16 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Parser where
 
-
-{-
-| So far this is Ctrl+c Ctrl+v
-| TODO :
-        impl polinome parsing and more unary and binary operators
-        clean up, maybe find easier way to do it
-        check if its correct
-        waaay laaater : lose the type parseing
--}
-
 import Core
+
 import Control.Applicative hiding (many, some)
 import Control.Monad
 import Data.Char
@@ -18,32 +11,74 @@ import Data.Void
 import Data.Functor
 import Text.Megaparsec
 import Text.Megaparsec.Debug (dbg)
+import Control.Monad.Combinators.Expr
+import Data.Text hiding (elem, empty)
 
 import qualified Text.Megaparsec.Char       as C
 import qualified Text.Megaparsec.Char.Lexer as L
 
--- parsing
---------------------------------------------------------------------------------
 
-type Parser = Parsec Void String
+{- AST : 
+
+--  Ennek a parsolása a pIdent ami bármilyen alphaNumerical Text ami nem keyword
+type Name = Text
+
+data TTm
+--  pLit és pVariable, ezek a leaf-jei egy Expr-nek
+    = TVar Name             -|
+    | TLit Literal          -|
+
+--  pLam
+    | TLam Name Type TTm       -|
+--  pLet
+    | TLet Name TTm TTm        -|
+
+--  Expr amiket a makeExprParserrel meg lehetne oldani
+    | TApp TTm TTm             -|
+    | TPlus TTm TTm             |
+    | TTimes TTm TTm            |
+    | TAnd  TTm TTm             |
+    | TOr  TTm TTm             -|
+    deriving Show
+
+data Type 
+--  pTBase    
+    = TBase
+
+--  tExpr
+    | TArr Type Type
+
+-}
+
+-- TODO: Change it to Text
+type Parser = Parsec Void Text
 
 ws :: Parser ()
 ws = L.space C.space1 (L.skipLineComment "--") (L.skipBlockComment "{-" "-}")
 
 lexeme :: Parser a -> Parser a
-lexeme     = L.lexeme ws
+lexeme = L.lexeme ws
 
-symbol :: String -> Parser String
-symbol s   = lexeme (C.string s)
+symbol :: Text -> Parser Text
+symbol s = lexeme (C.string s)
 
 char :: Char -> Parser Char
-char c     = lexeme (C.char c)
+char c = lexeme (C.char c)
 
 parens :: Parser a -> Parser a
 parens p   = char '(' *> p <* char ')'
 
-keyword :: String -> Bool
-keyword x = x == "\\" || x == "in" || x == "let"
+keywords :: [Name]
+keywords = ["\\", "let"]
+
+keyword :: Text -> Bool
+keyword x = x `elem` keywords
+
+pKeyword :: Text -> Parser ()
+pKeyword kw = do
+    void $ C.string kw
+    (takeWhile1P Nothing isAlphaNum *> empty) <|> ws
+
 
 -- TODO : this is magic i should ask what it does
 pIdent :: Parser Name
@@ -52,59 +87,8 @@ pIdent = try $ do
     guard (not (keyword x))
     x <$ ws
 
-pKeyword :: String -> Parser ()
-pKeyword kw = do
-    void $ C.string kw
-    (takeWhile1P Nothing isAlphaNum *> empty) <|> ws
-
-pBind :: Parser String
-pBind  = pIdent <|> symbol "_"
-
-pAtom :: Parser TTm --try might be needed here, or choice
-pAtom  = pLit <|> (TVar <$> pIdent) <|> parens pTm
-
-pSpine :: Parser TTm
-pSpine = foldl1 TApp <$> some pAtom
-
--- should work like : 
--- <type> ::= "Int" | "Bool" | <type> "->" <type>
--- type = pTypeLit optional ("->" type)
--- pTypeLit = "Int" | "Bool" | "(" type ")"
---
-
-pType :: Parser Type
-pType =  do 
-    t <- pBaseType
-    t' <- optional $ do 
-        void $ symbol "->"
-        pType
-    case t' of
-        Just a  -> return $ TArr t a
-        Nothing -> return t
-
-pBaseType :: Parser Type
-pBaseType = choice
-    [ symbol "Int"  $> TInt
-    , symbol "Bool" $> TBool
-    , parens pType
-    ]
-
---pType :: Parser Type
---pType  = pArr <|> (symbol "Int" *> return TInt) <|> (symbol "Bool" *> return TBool)
-
-{-
-pArr :: Parser Type
-pArr = do
-    void $ char '('
-    a <- pType
-    void $ symbol "->"
-    b <- pType
-    void $ char ')'
-    return $ TArr a b
--}
-
-pBinAll :: Parser TTm
-pBinAll = choice $ map (try . pBinOp) ['+','*','|','&']
+pVariable :: Parser TTm
+pVariable = TVar <$> pIdent
 
 pLit :: Parser TTm
 pLit = pInt <|> pBool
@@ -112,90 +96,117 @@ pLit = pInt <|> pBool
 pInt :: Parser TTm
 pInt = do
     i <- lexeme L.decimal
-    return $ TLit $ LInt i
+    return $ TLit $ LNumber i
 
 pBool :: Parser TTm
 pBool = choice
         [ TLit (LBool True)  <$ symbol "true"
         , TLit (LBool False) <$ symbol "false"]
 
-{-
-pBool = do
-    b <- (symbol "true" <|> symbol "false")
-    pure $ TLit $ LBool (if b == "true" then True else False)
--}
+operatorTable :: [[Operator Parser TTm]]
+operatorTable =
+  [ 
+    [
+        binaryL ""  TApp
+    ] ,
+    [
+      prefix  "-"   TNeg
+    ] ,
+    [
+      binaryL "*"   TTimes
+    , binaryL "/"  TDiv
+    , binaryL "div" TIntDiv
+    , binaryL "mod" TMod
+    , binaryL "="   TEq
+    ] ,
+    [ 
+      binaryL "+"   TPlus
+    , binaryL "-"   TMinus
+    , binaryL "|"   TOr
+    , binaryL "&"   TAnd
+    , binaryL "^"   TPow
+    , binaryL "<="  TLte
+    , binaryL ">="  TGte
+    , binaryL "<"   TLt
+    , binaryL ">"   TGt
+    ]
+  ]
 
-pBinAtom :: Parser TTm
-pBinAtom = pLit <|> parens pTm
+binaryL :: Text -> (a -> a -> a) -> Operator Parser a
+binaryL n f = InfixL (f <$ symbol n)
 
--- A bug where : \x : Int . x + 3 would not parse
-pBinOp :: Char -> Parser TTm
-pBinOp op = do
-    --a <- pBinAtom  
-    a <- pAtom
-    void $ lexeme (C.char op)
-    b <- pTm
-    return $ h op a b
-    where
-        h '+' = TPlus
-        h '*' = TTimes
-        h '&' = TAnd
-        h '|' = TOr
-        h _   = error "Unsupported operator "
-    -- Throw errors
+binaryR :: Text -> (a -> a -> a) -> Operator Parser a
+binaryR n f = InfixR (f <$ symbol n)
 
+prefix, postfix :: Text -> (a -> a) -> Operator Parser a
+prefix  n f = Prefix  (f <$ symbol n)
+postfix n f = Postfix (f <$ symbol n)
+
+-- A Expr is a tree with nodes (+), (*), ... and leafes pLit, or pVariable
+pExprT :: Parser TTm
+pExprT = try $ choice 
+    [ 
+      {-dbg "parens expr" -} (parens pExpr) <?> "parens expr"
+    , {-dbg "literal"     -} pLit           <?> "literal"
+    , {-dbg "variable"    -} pVariable      <?> "variable"
+    ]
+
+pExpr :: Parser TTm
+pExpr = makeExprParser pExprT operatorTable
+
+pBind :: Parser Text
+pBind  = pIdent <|> symbol "_"
+
+-- TODO : Lam could be a prefix operator
+--pLam :: Parser TTm
 pLam :: Parser TTm
 pLam = do
     void $ char '\\'
-    x <- pBind  --some pBind
+    x <- pBind
     void $ char ':'
     t <- pType
     void $ symbol "."
     u <- pTm
-    return $ TLam x t u  --foldr Lam t xs
+    return $ TLam x t u
 
+
+-- TODO : Let could also be a prefix operator
 pLet :: Parser TTm
 pLet = do
     pKeyword "let"
     x <- pBind
-    void $ symbol "="
+    void $ symbol ":="
     t <- pTm
     void $ symbol ";"
     u <- optional pTm
     return $ case u of
         Just u' -> TLet x t u'
         Nothing -> TLet x t $ TLit LTop
---    return $ TLet x t u
+
+pBaseType :: Parser Type
+pBaseType = choice
+    [ symbol "Num"  $> TNumber
+    , symbol "Bool" $> TBool
+    , symbol "Top"  $> TTop
+    , parens pType
+    ]
+
+operatorTableT :: [[Operator Parser Type]]
+operatorTableT = [[binaryR "->" TArr]]
+
+pType :: Parser Type
+pType = makeExprParser pBaseType operatorTableT
 
 pTm :: Parser TTm
-pTm  = choice
-    [ {-dbg "bin"    -} pBinAll
-    , {-dbg "literal"-} pLit
-    , {-dbg "lambda" -} pLam 
-    , {-dbg "letbind"-} pLet  
-    , {-dbg "app"    -} pSpine ] <?> "a valid term"
-
+pTm  = try (choice
+    [ {-dbg "expr"   -} pExpr
+    , {-dbg "lambda" -} pLam
+    , {-dbg "letbind"-} pLet
+    , {-dbg "letbind"-} parens pTm
+    ] <?> "a valid term")
 
 pSrc :: Parser TTm
 pSrc = ws *> pTm <* eof
 
-parseString :: String -> Either (ParseErrorBundle String Void) TTm
+parseString :: Text -> Either (ParseErrorBundle Text Void) TTm
 parseString src = parse pSrc "(stdin)" src
-
-{-
-parseStringAndPrint :: String -> IO TTm
-parseString src =
-    case parse pSrc "(stdin)" src of
-        Left e -> do
-            putStrLn $ errorBundlePretty e
-            exitFailure
-        Right t ->
-            pure t
--}
-
--- Test
-
-ptest1 = parseString "let intId = (f x : Int -> x); (intId 3)"
-
-mTm :: String
-mTm = "f y : (Bool -> Int) . f x : ((Bool -> Int) -> (Int -> Bool)) . x y"
---
+++ poly_lang/app/ParserDepr.hs
@@ -0,0 +1,201 @@
+module Parser where
+
+
+{-
+| So far this is Ctrl+c Ctrl+v
+| TODO :
+        impl polinome parsing and more unary and binary operators
+        clean up, maybe find easier way to do it
+        check if its correct
+        waaay laaater : lose the type parseing
+-}
+
+import Core
+import Control.Applicative hiding (many, some)
+import Control.Monad
+import Data.Char
+import Data.Void
+import Data.Functor
+import Text.Megaparsec
+import Text.Megaparsec.Debug (dbg)
+
+import qualified Text.Megaparsec.Char       as C
+import qualified Text.Megaparsec.Char.Lexer as L
+
+-- parsing
+--------------------------------------------------------------------------------
+
+type Parser = Parsec Void String
+
+ws :: Parser ()
+ws = L.space C.space1 (L.skipLineComment "--") (L.skipBlockComment "{-" "-}")
+
+lexeme :: Parser a -> Parser a
+lexeme     = L.lexeme ws
+
+symbol :: String -> Parser String
+symbol s   = lexeme (C.string s)
+
+char :: Char -> Parser Char
+char c     = lexeme (C.char c)
+
+parens :: Parser a -> Parser a
+parens p   = char '(' *> p <* char ')'
+
+keyword :: String -> Bool
+keyword x = x == "\\" || x == "in" || x == "let"
+
+-- TODO : this is magic i should ask what it does
+pIdent :: Parser Name
+pIdent = try $ do
+    x <- takeWhile1P Nothing isAlphaNum
+    guard (not (keyword x))
+    x <$ ws
+
+pKeyword :: String -> Parser ()
+pKeyword kw = do
+    void $ C.string kw
+    (takeWhile1P Nothing isAlphaNum *> empty) <|> ws
+
+pBind :: Parser String
+pBind  = pIdent <|> symbol "_"
+
+pAtom :: Parser TTm --try might be needed here, or choice
+pAtom  = pLit <|> (TVar <$> pIdent) <|> parens pTm
+
+pSpine :: Parser TTm
+pSpine = foldl1 TApp <$> some pAtom
+
+-- should work like : 
+-- <type> ::= "Int" | "Bool" | <type> "->" <type>
+-- type = pTypeLit optional ("->" type)
+-- pTypeLit = "Int" | "Bool" | "(" type ")"
+--
+
+pType :: Parser Type
+pType =  do 
+    t <- pBaseType
+    t' <- optional $ do 
+        void $ symbol "->"
+        pType
+    case t' of
+        Just a  -> return $ TArr t a
+        Nothing -> return t
+
+pBaseType :: Parser Type
+pBaseType = choice
+    [ symbol "Int"  $> TInt
+    , symbol "Bool" $> TBool
+    , parens pType
+    ]
+
+--pType :: Parser Type
+--pType  = pArr <|> (symbol "Int" *> return TInt) <|> (symbol "Bool" *> return TBool)
+
+{-
+pArr :: Parser Type
+pArr = do
+    void $ char '('
+    a <- pType
+    void $ symbol "->"
+    b <- pType
+    void $ char ')'
+    return $ TArr a b
+-}
+
+pBinAll :: Parser TTm
+pBinAll = choice $ map (try . pBinOp) ['+','*','|','&']
+
+pLit :: Parser TTm
+pLit = pInt <|> pBool
+
+pInt :: Parser TTm
+pInt = do
+    i <- lexeme L.decimal
+    return $ TLit $ LInt i
+
+pBool :: Parser TTm
+pBool = choice
+        [ TLit (LBool True)  <$ symbol "true"
+        , TLit (LBool False) <$ symbol "false"]
+
+{-
+pBool = do
+    b <- (symbol "true" <|> symbol "false")
+    pure $ TLit $ LBool (if b == "true" then True else False)
+-}
+
+pBinAtom :: Parser TTm
+pBinAtom = pLit <|> parens pTm
+
+-- A bug where : \x : Int . x + 3 would not parse
+pBinOp :: Char -> Parser TTm
+pBinOp op = do
+    --a <- pBinAtom  
+    a <- pAtom
+    void $ lexeme (C.char op)
+    b <- pTm
+    return $ h op a b
+    where
+        h '+' = TPlus
+        h '*' = TTimes
+        h '&' = TAnd
+        h '|' = TOr
+        h _   = error "Unsupported operator "
+    -- Throw errors
+
+pLam :: Parser TTm
+pLam = do
+    void $ char '\\'
+    x <- pBind  --some pBind
+    void $ char ':'
+    t <- pType
+    void $ symbol "."
+    u <- pTm
+    return $ TLam x t u  --foldr Lam t xs
+
+pLet :: Parser TTm
+pLet = do
+    pKeyword "let"
+    x <- pBind
+    void $ symbol "="
+    t <- pTm
+    void $ symbol ";"
+    u <- optional pTm
+    return $ case u of
+        Just u' -> TLet x t u'
+        Nothing -> TLet x t $ TLit LTop
+--    return $ TLet x t u
+
+pTm :: Parser TTm
+pTm  = choice
+    [ {-dbg "bin"    -} pBinAll
+    , {-dbg "literal"-} pLit
+    , {-dbg "lambda" -} pLam 
+    , {-dbg "letbind"-} pLet  
+    , {-dbg "app"    -} pSpine ] <?> "a valid term"
+
+
+pSrc :: Parser TTm
+pSrc = ws *> pTm <* eof
+
+parseString :: String -> Either (ParseErrorBundle String Void) TTm
+parseString src = parse pSrc "(stdin)" src
+
+{-
+parseStringAndPrint :: String -> IO TTm
+parseString src =
+    case parse pSrc "(stdin)" src of
+        Left e -> do
+            putStrLn $ errorBundlePretty e
+            exitFailure
+        Right t ->
+            pure t
+-}
+
+-- Test
+
+ptest1 = parseString "let intId = (f x : Int -> x); (intId 3)"
+
+mTm :: String
+mTm = "f y : (Bool -> Int) . f x : ((Bool -> Int) -> (Int -> Bool)) . x y"
