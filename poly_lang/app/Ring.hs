20e94c328d253561032e98ac3626bbe2ad6312d7
IModer <korpa.peter0723@gmail.com>
2024-03-22 13:56:44 +0100

    Added semiring interface

--- poly_lang/app/Ring.hs
+++ poly_lang/app/Ring.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE DataKinds #-}
+
 module Ring where
 
-import Prelude hiding (mod, div)
+import Prelude hiding (mod, div, negate)
 import Data.Ratio
-import Data.Poly
+import Data.Poly.Multi.Semiring 
+import Data.Semiring hiding ((+), (-), (*), (^), fromInteger)
+import GHC.TypeNats
 
 --TODO: move this to some lib
 zipWithPad :: (a -> b -> c) -> a -> b -> [a] -> [b] -> [c]
@@ -16,6 +20,16 @@ data Complex a = !a :+ !a
     deriving (Eq, Show)
 
 infix 7 :+
+
+--instance (Show a) => Show (Complex a) where
+--    show (a :+ b) = show a ++ "+" ++ show b ++ "i"
+{-
+instance Show (Complex Rational) where
+    show (a :+ b) = showa  ++ "+" ++ showb ++ "i"
+        where
+            showa = "(" ++ show (numerator a) ++ "/" ++ show (denominator a) ++ ")"
+            showb = "(" ++ show (numerator b) ++ "/" ++ show (denominator b) ++ ")"
+-}
 
 conjugate :: (Num a) => Complex a -> Complex a
 conjugate (a :+ b) = a :+ (- b)
@@ -51,6 +65,18 @@ signumC (a :+ b) = signum a :+ 0
 fromIntegerC :: (Num a) => Integer -> Complex a
 fromIntegerC a = fromInteger a :+ 0
 
+instance Ring a => Semiring (Complex a) where
+  zero = zero :+ zero
+  one  = one  :+ zero
+  plus  (x :+ y) (x' :+ y') = plus x x' :+ plus y y'
+  times (x :+ y) (x' :+ y')
+    = (x `times` x' `plus` (negate (y `times` y'))) :+ (x `times` y' `plus` y `times` x')
+  fromNatural n = fromNatural n :+ zero
+{-
+instance Ring Rational where
+    negate = (0-)
+-}
+
 instance (Num a) => Num (Complex a) where
     (+) = (/+/)
     (*) = (/*/)
@@ -64,15 +90,28 @@ instance (Num a) => Num (Complex a) where
 (///) z@(a :+ b) w@(c :+ d) = ((a * c + b * d) / denom ) :+ ((b * c - a * d) / denom)
     where denom = c ^ 2 + d ^ 2
 
+instance (Fractional a, Num a) => Fractional (Complex a) where
+    (/) = (///)
+    fromRational r = (fromRational r :+ 0)
+
 (.^) :: (Num a, Integral a) => Complex a -> a -> Complex a
 (.^) z@(a :+ b) n | n <= 0    = 1 :+ 0
                   | otherwise = z * (z .^ (n - 1))
 
-mod :: Complex a -> Complex a -> Complex a
-mod = undefined
+modCR :: Complex Rational -> Complex Rational -> Complex Rational
+modCR z@(a :+ b) w@(c :+ d) = z - w * floorc (z / w)
+    where
+        floorc (x :+ y) = (round x % 1) :+ (round y % 1)
 
-div :: Complex a -> Complex a -> Complex a
-div = undefined
+div :: (Integral a,  RealFrac a) => Complex a -> Complex a -> Complex a
+div z@(a :+ b) w@(c :+ d) = undefined
+-- Fractional mod a la https://functions.wolfram.com/IntegerFunctions/Mod/27/01/01/
+-- We the take the whole park mod that then add the fractional part 
+modR :: Rational -> Rational -> Rational
+modR m n = m - n * (floor (m / n) % 1)
+
+divR :: Rational -> Rational -> Rational
+divR a b = undefined
 
 -- Also need Ord and Eq for all numbers, polinomials
 --                                  Real
@@ -107,14 +146,24 @@ b' =  (2 % 3) :+ (1 % 3)
     "^"
 -}
 
-test :: [N -> N -> N]
-test = 
+testRational :: [Rational -> Rational -> Rational]
+testRational = 
+    [ (+)
+    , (-)
+    , (*)
+    , (/)
+    , modR
+    , divR ]
+--    (^)
+
+testComplex :: [N -> N -> N]
+testComplex = 
     [ (/+/)
-    , (/*/)
     , (/-/)
+    , (/*/)
     , (///)
-    , div
-    , mod
+-- í   , div
+    , modCR
 --        (.^)
         ]
 
@@ -126,11 +175,13 @@ test =
 --      or more generally, for all 
 --      Field/Ring a => Field/Ring (Complex a)
 
-p1 :: VPoly (Complex Rational)
-p1 = 2 * X + 3
+--p1 :: VMultiPoly 3 (Complex Rational)
+--p1 = 2 * X + 3
 
-p2 :: VPoly (Complex Rational)
-p2 = X * X + X
+p2 :: MPoly 1
+p2 = 1 + X
+
+type MPoly (n :: Nat) = VMultiPoly n (Complex Rational)
 
 factor :: Number -> Number
 factor a = undefined
@@ -140,3 +191,12 @@ irred a = undefined
 
 derivative :: Number -> Number
 derivative a = undefined
+
+{-
+-}
+testPoly :: [MPoly 1 -> MPoly 1 -> MPoly 1]
+testPoly = 
+    [ (+)
+    , (-)
+    , (*)
+    ]
