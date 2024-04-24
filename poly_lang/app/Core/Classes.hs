a806c9acbde72d9c1a225c65106a9a95da8ebe88
IModer <korpa.peter0723@gmail.com>
2024-04-24 14:02:20 +0200

    Added real and imag

--- poly_lang/app/Core/Classes.hs
+++ poly_lang/app/Core/Classes.hs
@@ -10,12 +10,11 @@ import Data.Text hiding (map, all)
 import Data.Maybe (isJust)
 
 -- Error type for typechecking errors
--- is a monad
 
 type ErrorT = ExceptT String
 
-throwErrorLift :: MonadError e m' => e -> m' a 
-throwErrorLift = liftEither . throwError
+--throwErrorLift :: MonadError e m' => e -> m' a 
+--throwErrorLift = liftEither . throwError
 
 -- Custome state for type context and value context
 -- As long as the getter and setter functions are used 
@@ -59,13 +58,29 @@ data GEnv = GEnv { typeEnv :: TEnv
                  , zmodf :: Maybe (PolyMulti Frac) }
 -}
 
+imag'' :: Val -> ErrorT GState Val
+imag'' (VCNum p) = return $ VCNum (imag p :+ 0 %% 1)
+imag'' a         = return $ VVar (pack "builtin.imag") ... VVar (pack "x")
+
+imagType = (pack "imag", TCNum ~> TCNum)
+imagVal  = (pack "imag", VLam (pack "x") TCNum $ \x ->  
+                            imag'' x)
+
+real'' :: Val -> ErrorT GState Val
+real'' (VCNum p) = return $ VCNum (real p :+ 0 %% 1)
+real'' a         = return $ VVar (pack "builtin.real") ... VVar (pack "x")
+
+realType = (pack "real", TCNum ~> TCNum)
+realVal  = (pack "real", VLam (pack "x") TCNum $ \x ->  
+                            real'' x)
+
 irred'' :: Val -> ErrorT GState Val
 irred'' (VCPoly p) = if polyIsZx p
                         then case monoPolyToZx p of
-                            Just (p' , i) -> return $ VBool $ (irred' p')
+                            Just (p' , i) -> return $ VBool $ irred' p'
                             Nothing       -> throwError "Runtime error : Argument to irred should be a polinomian with a single varable and coeffs in Z"
                         else throwError "Runtime error : Argument to irred should be a polinomian with a single varable and coeffs in Z"
-irred'' a          = return $ (VVar $ pack "builtin.irred") `VApp` (VVar $ pack "p")
+irred'' a          = return $ VVar (pack "builtin.irred") ... VVar (pack "p")
 
 irredType = (pack "irred", TCPoly ~> TBool)
 irredVal  = (pack "irred", VLam (pack "p") TCPoly $ \p ->  
@@ -74,10 +89,10 @@ irredVal  = (pack "irred", VLam (pack "p") TCPoly $ \p ->
 factor'' :: Val -> ErrorT GState Val
 factor'' (VCPoly p) = if polyIsZx p
                         then case monoPolyToZx p of
-                            Just (p' , i) -> return $ VList $ listToList $ map (VCPoly . (flip zxToMultiPoly) i) (factor' p')
+                            Just (p' , i) -> return $ VList $ listToList $ map (VCPoly . flip zxToMultiPoly i) (factor' p')
                             Nothing       -> throwError "Runtime error : Argument to factor should be a polinomian with a single varable and coeffs in Z"
                         else throwError "Runtime error : Argument to factor should be a polinomian with a single varable and coeffs in Z"
-factor'' a          = return $ (VVar $ pack "builtin.factor") `VApp` (VVar $ pack "p")
+factor'' a          = return $ VVar (pack "builtin.factor") ... VVar (pack "p")
 
 factorType = (pack "factor", TCPoly ~> TList)
 factorVal  = (pack "factor", VLam (pack "p") TCPoly $ \p ->  
@@ -96,7 +111,7 @@ eval'' (VList l) (VCPoly p) = let l' = listToEvalInp $ listToList' l in
                                 if l' /= [] && all ((`loneVarOf` p) . fst) l' 
                                     then return $ VCNum $ eval' l' p
                                     else throwError "Runtime error : List given to eval must have the appopriate format : TODO"
-eval'' a          b         = return $ ((VVar $ pack "builtin.eval") `VApp` (VVar $ pack "ls")) `VApp` (VVar $ pack "p")
+eval'' a          b         = return $ (VVar (pack "builtin.eval") ... VVar (pack "ls")) ... VVar (pack "p")
 
 evalType = (pack "eval", TList ~> TCPoly ~> TCNum)
 evalVal  = (pack "eval", VLam (pack "ls") TList $ \ls -> 
@@ -110,7 +125,7 @@ deriv'' (VCPoly i) (VCPoly p) = if loneVarOf i p
                                          derivativeVar i p
                                     else throwError "Runtime error : Variable to take derivative in must be a single variable thats present in the polinome"
 deriv'' a         b          = return $ 
-                                ((VVar $ pack "builtin.derivative") `VApp` (VVar $ pack "x")) `VApp` (VVar $ pack "p")
+                                (VVar (pack "builtin.derivative") ... VVar (pack "x")) ... VVar (pack "p")
 
 derivType = (pack "derivative", TCPoly ~> TCPoly ~> TCPoly)
 derivVal  = (pack "derivative", VLam (pack "x") TCPoly $ \x ->
@@ -118,21 +133,9 @@ derivVal  = (pack "derivative", VLam (pack "x") TCPoly $ \x ->
                             deriv'' x p)
 
 emptyEnv :: GEnv
-emptyEnv = GEnv [derivType, evalType, factorType, irredType]
-                [derivVal, evalVal, factorVal, irredVal] Nothing Nothing
+emptyEnv = GEnv [derivType, evalType, factorType, irredType, realType, imagType]
+                [derivVal, evalVal, factorVal, irredVal, realVal, imagVal] Nothing Nothing
 
 type GState = State GEnv
 
 type GStateT = StateT GEnv
-
-{-
-instance Monoid (ExceptT String GState String) where
-    mempty = return ""
-    mappend = (<>)
-
-instance Semigroup (ExceptT String GState String) where
-    a <> b = do
-        a' <- a
-        b' <- b
-        return $ a' ++ b'
--}
